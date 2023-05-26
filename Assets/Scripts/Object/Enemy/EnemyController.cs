2dfae0c1f6913e9c3db902d19e748ad83f0ae8cf
bloges56 <brady.drew.logan@gmail.com>
2023-05-26 08:50:05 -0500

    able to spawn and destroy enemies which also performs the corresponding actions in the org

---
+++ Assets/Scripts/Object/Enemy/EnemyController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    GameObject player;
+    [SerializeField] int speed;
+    EnemySpawner spawner;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        spawner = GameObject.Find("EnemySpawner").GetComponent<EnemySpawner>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(player.name);
+        transform.position += Vector3.Normalize(player.transform.position - transform.position) * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision Detected");
+        if(collision.gameObject.tag == "Bullet")
+        {
+            spawner.RemoveEnemy(this.gameObject);
+        }
+    }
+}
